ee098177a4affc42472653d8603b79f5d8463c7b
root <root@Lucawin.>
2024-11-26 13:49:59 +0100

    update print

--- print.c
+++ print.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 
 int _printf(const char *format, ...);
-{ 
-	va_list args;
-	int count = 0;
-	const char *ptr;
-	
-	va_start(args, format);
+{
+va_list args;
+int count = 0;
+const char *ptr;
+
+va_start(args, format);
 
 for (ptr = format; *ptr != '/0'; ptr++)
 {
@@ -20,7 +20,7 @@ char ch = (char) va_arg(args, int);
 putchar(ch);
 count++;
 }
-else if (*ptr == 's') 
+else if (*ptr == 's')
 {
 char *str = va_arg(args, char*)
 if (str != NULL)
@@ -30,27 +30,31 @@ while (*str)
 putchar(*str++);
 count++;
 }
-  } else {  // Si la chaîne est NULL
-                    fputs("(null)", stdout);  // Afficher "(null)"
-                    count += 6;  // "(null)" a 6 caractères
-                }
-            } else if (*ptr == '%') {  // Traitement du spécificateur '%%' (pourcentage)
-                putchar('%');
-                count++;
-            }
-        } else {  // Si ce n'est pas un '%', on affiche le caractère tel quel
-            putchar(*ptr);
-            count++;
-        }
-    }
-
-    va_end(args);  // Libération de la liste des arguments
-    return count;  // Retourne le nombre total de caractères imprimés
+} 
+else 
+{  // if the string is null
+fputs("(null)", stdout);  // Post "(null)"
+count += 6;  // "(null)" a 6 caractères
+}
+} else if (*ptr == '%') {  //Processing of specifier '%%' (percentage)
+putchar('%');
+count++;
+}
+} else
+{  // If it is not a '%', the character is displayed as is
+putchar(*ptr);
+count++;
+}
 }
 
-int main() {
-    // Exemple d'utilisation
-    int printed = _printf("Bonjour, %s! Caractère : %c, Pourcentage : %%\n", "Monde", 'A');
-    printf("Nombre de caractères imprimés : %d.\n", printed);  // Affiche le nombre de caractères imprimés
-    return 0;
+va_end(args);  // Releasing the list of arguments
+return count;  // Returns the total number of characters printed
+}
+
+int main() 
+{
+// Example of use
+int printed = _printf("Bonjour, %s! Caractère : %c, Pourcentage : %%\n", "Monde", 'A');
+printf("Nombre de caractères imprimés : %d.\n", printed); // Displays the number of characters printed
+return 0;
 }

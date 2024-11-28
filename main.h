dda334256a9f8469111efe22570f5ea536aac1a7
korneevscp <vladimirivanof2024@gmail.com>
2024-11-28 14:09:39 +0100

    testing

--- main.h
+++ main.h
@@ -1,26 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
-
-
 /**
 * struct format - match the conversion specifiers to args passed
 * @id: pointer to format specifer, i.e. id[0] = % and id[1] is the letter (c, s, d, i)
 * @f: pointer to function determined by the conversion specifier
 *
 */
-
 typedef struct format
 {
 char *id;
 int (*f)();
 } convert_match;
-
 int printf_int(va_list args);
 int printf_dec(va_list args);
 int *_strcpy(char *dest, char *src);

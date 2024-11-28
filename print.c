4615420bcb7131d339032c4a032c88af749e5f63
root <root@Lucawin.>
2024-11-28 12:32:28 +0100

    update printf.c

--- print.c
+++ print.c
@@ -1,68 +1,9 @@
-#include"main.h"
-#include<stdio.h>
-//* texte à rajouter
+#include"MAIN.H"
+
+
 
 int _printf(const char *format, ...);
 {
-va_list args;
-int count = 0;
-const char *ptr;
-
-va_start(args, format);
-
-for (ptr = format; *ptr != '/0'; ptr++)
-{
-if (*ptr == '%')
-{
-ptr++;
-if (*ptr == 'c')
-{
-char ch = (char) va_arg(args, int);
-putchar(ch);
-count++;
-}
-else if (*ptr == 's')
-{
-char *str = va_arg(args, char*)
-if (str != NULL)
-{
-while (*str)
-{
-putchar(*str++);
-count++;
-}
-}
-else
-{
-
-fputs("(null)", stdout);
-
-"(null)"
-count += 6;
-
-}
-{
-else if (*ptr == '%')
-{
-putchar('%');
-count++;
-}
-}
-else
-{
-putchar(*ptr);
-count++;
-}
-}
-
-va_end(args);
-return (count);
-}
-
-int main(void)
-{
-
-int printed = _printf("Bonjour, %s! Caractère : %c, Pourcentage : %%\n", "Monde", 'A');
-printf("Nombre de caractères imprimés : %d.\n", printed);
-return (0);
+	(void) format;
+	return 0;
 }
